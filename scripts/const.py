a04d9538908cc5ba016756f173c9dfb124b854e2
Felippe Andrade <4drade@gmail.com>
2024-02-24 12:24:07 -0300

    feat: Add selector constants to const.py
    The const.py script is newly introduced with specific dictionary SELECTORS. These selectors are used for web scraping specific elements from the target website. By centralizing these selectors to one/constants.py script, it improves the maintainability of the code.

---
+++ scripts/const.py
@@ -0,0 +1,15 @@
+SELECTORS = dict(
+    offer_title="#\:Raidq\: > div.andes-carousel-snapped__controls-wrapper > div > div > div > div > "
+                "div.poly-card__content > a",
+    original_price="#\:Raidq\: > div.andes-carousel-snapped__controls-wrapper > div > div > div > div > "
+                   "div.poly-card__content > div.poly-component__price > s",
+    current_price="#\:Raidq\: > div.andes-carousel-snapped__controls-wrapper > div > div > div > div > "
+                  "div.poly-card__content > div.poly-component__price > div > "
+                  "span.andes-money-amount.andes-money-amount--cents-superscript",
+    amount_discount="#\:Raidq\: > div.andes-carousel-snapped__controls-wrapper > div > div > div > div > "
+                    "div.poly-card__content > div.poly-component__price > div > span.andes-money-amount__discount",
+    installments="#\:Raidq\: > div.andes-carousel-snapped__controls-wrapper > div > div > div > div > "
+                 "div.poly-card__content > div.poly-component__price > span",
+    shipping="#\:Raidq\: > div.andes-carousel-snapped__controls-wrapper > div > div > div > div > "
+             "div.poly-card__content > div.poly-component__shipping",
+)
