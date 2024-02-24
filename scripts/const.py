ffe7d30d4633804a673735a2a01fbb46322953c9
Felippe Andrade <4drade@gmail.com>
2024-02-24 17:57:59 -0300

    refactor: CSS selector constants for improved readability and maintenance
    Consolidate and format CSS selector constants for better code organization and readability. This simplifies the maintenance process and promotes code consistency.

--- scripts/const.py
+++ scripts/const.py
@@ -1,15 +1,15 @@
 SELECTORS = dict(
-    offer_title="#\:Raidq\: > div.andes-carousel-snapped__controls-wrapper > div > div > div > div > "
-                "div.poly-card__content > a",
-    original_price="#\:Raidq\: > div.andes-carousel-snapped__controls-wrapper > div > div > div > div > "
-                   "div.poly-card__content > div.poly-component__price > s",
-    current_price="#\:Raidq\: > div.andes-carousel-snapped__controls-wrapper > div > div > div > div > "
-                  "div.poly-card__content > div.poly-component__price > div > "
-                  "span.andes-money-amount.andes-money-amount--cents-superscript",
-    amount_discount="#\:Raidq\: > div.andes-carousel-snapped__controls-wrapper > div > div > div > div > "
-                    "div.poly-card__content > div.poly-component__price > div > span.andes-money-amount__discount",
-    installments="#\:Raidq\: > div.andes-carousel-snapped__controls-wrapper > div > div > div > div > "
-                 "div.poly-card__content > div.poly-component__price > span",
-    shipping="#\:Raidq\: > div.andes-carousel-snapped__controls-wrapper > div > div > div > div > "
-             "div.poly-card__content > div.poly-component__shipping",
+    offer_title=r"#\:Raidq\: > div.andes-carousel-snapped__controls-wrapper > div > div > div > div > "
+    "div.poly-card__content > a",
+    original_price=r"#\:Raidq\: > div.andes-carousel-snapped__controls-wrapper > div > div > div > div > "
+    "div.poly-card__content > div.poly-component__price > s",
+    current_price=r"#\:Raidq\: > div.andes-carousel-snapped__controls-wrapper > div > div > div > div > "
+    "div.poly-card__content > div.poly-component__price > div > "
+    "span.andes-money-amount.andes-money-amount--cents-superscript",
+    amount_discount=r"#\:Raidq\: > div.andes-carousel-snapped__controls-wrapper > div > div > div > div > "
+    "div.poly-card__content > div.poly-component__price > div > span.andes-money-amount__discount",
+    installments=r"#\:Raidq\: > div.andes-carousel-snapped__controls-wrapper > div > div > div > div > "
+    "div.poly-card__content > div.poly-component__price > span",
+    shipping=r"#\:Raidq\: > div.andes-carousel-snapped__controls-wrapper > div > div > div > div > "
+    "div.poly-card__content > div.poly-component__shipping",
 )
